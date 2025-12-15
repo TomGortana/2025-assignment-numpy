d79509f13360b6efdb90c20e899c137bb29e3da7
Tom <tom.gortana@gmail.com>
2025-12-15 14:51:43 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -11,7 +11,7 @@ complete the assignment. The code should be able to pass the test that we
 wrote. To run the tests, use `pytest test_numpy_question.py` at the root of
 the repo. It should say that 2 tests ran with success.
 
-We also ask to respect the pep8 convention: https://pep8.org.
+We also ask to respect the pep8 convention: https:// pep8.org.
 This will be enforced with `flake8`. You can check that there is no flake8
 errors by calling `flake8` at the root of the repo.
 """
@@ -37,10 +37,14 @@ def max_index(X):
         If the input is not a numpy array or
         if the shape is not 2D.
     """
+
     i = 0
     j = 0
 
-    # TODO
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise ValueError("X must be a numpy array and 2D.")
+
+    i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
     return i, j
 
@@ -64,4 +68,13 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if n_terms < 0:
+        raise ValueError("n_terms must be non-negative")
+    if n_terms == 0:
+        return 2.0
+
+    n = np.arange(1, n_terms + 1)
+    factors = (2 * n / (2 * n - 1.0)) * (2 * n / (2 * n + 1.0))
+    product = np.prod(factors)
+
+    return 2.0 * product
